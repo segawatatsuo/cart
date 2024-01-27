08e240f3e067f9b68f9ae54c88c5d3bf66b30e34
segawa <segawa@lookingfor.jp>
2024-01-27 22:53:47 +0900

    1-27 22:53

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -268,7 +268,7 @@
     <meta name="_token" content="{{ csrf_token() }}" />
     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/min/dropzone.min.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/dropzone.js"></script>
     {{-- dropzone用に追加 --}}
 
